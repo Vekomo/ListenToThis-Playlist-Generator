c51b50d99b69398159d41c014d2a48ef10a2bd15
Kleoto <pathanfaraaz@gmail.com>
2017-07-08 19:51:47 -0700

    Set up config file and parser

--- ListenToThisGen.py
+++ ListenToThisGen.py
@@ -4,14 +4,21 @@ import configparser
 import spotipy
 import spotipy.util as util
 
-username = ""
-scope = ''
-client_id = ''
-client_secret = ''
-redirect_uri = 'http://localhost'
+
+#Read from the config file for our credentials and keys
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# THe S_ denotes spotify, R_ for the reddit credentials
+S_USERNAME = config.get('spotify', 's_username')
+S_SCOPE = config.get('spotify', 's_scope')
+S_CLIENT_ID = config.get('spotify', 's_client_id')
+S_CLIENT_SECRET = config.get('spotify', 's_client_secret')
+S_REDIRECT_URI = config.get('spotify', 's_redirect_uri')
 
 #Gets access token to work with Spotify API
-token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
+token = util.prompt_for_user_token(S_USERNAME, S_SCOPE, S_CLIENT_ID, S_CLIENT_SECRET, S_REDIRECT_URI)
 
 #IF token request was succesful, "Do stuff". ELSE print out why failed
 if token:
